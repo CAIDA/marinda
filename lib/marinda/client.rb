798532ccf446d75938881dabbb780d049d197841
Young Hyun <youngh@caida.org>
2009-12-13 17:00:06 -0800

    Implemented basic asynchronous operations.
    Implemented read_async, readp_async, take_async, takep_async,
    take_priv_async, and takep_priv_async.

--- lib/marinda/client.rb
+++ lib/marinda/client.rb
@@ -38,9 +38,25 @@ class MarindaError < StandardError; end
 class OperationError < MarindaError; end
 class PrivilegeError < MarindaError; end
 class ProtocolError < MarindaError; end
+
+# A Marinda::Client object was improperly used by a client.  For example,
+# a client can't invoke another command (e.g., write) while an asynchronous
+# operation (e.g., take_async) is in progress.  The only exception is a
+# cancel command.  Similarly, a client can't invoke another command while
+# a (synchronous) iteration/consume opeation is in progress.
+class ClientError < MarindaError; end
+
 class ConnectionBroken < IOError; end
 
 class Client
+
+  @@debug = false
+
+  def self.debug=(v)
+    @@debug = v
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   include ChannelFlagReaders
   include ChannelMessageCodes
@@ -48,6 +64,11 @@ class Client
   # An arbitrarily chosen value representing the largest legal reqnum value
   # before wrapping.  This must be storable with Array#pack("C").
   REQNUM_MAX = 2**8 - 1
+
+  MESSAGE_LENGTH_SIZE = 2  # num bytes in length field of client msg
+  READ_SIZE = 8192         # num bytes to read at once with sysread
+
+  ReadBuffer = Struct.new :length, :payload
 
   attr_accessor :remote_banner, :debug
 
@@ -72,8 +93,36 @@ class Client
     @protocol = 0
     @remote_banner = nil
     @sock = sock
+
+    # This is
+    #
+    #  * nil for singleton operations (e.g., write, read, take),
+    #  * the command code for synchronous iteration and consume operations, 
+    #    (e.g., READ_ALL_CMD, MONITOR_STREAM_CMD), and
+    #  * the corresponding Method object from @dispatch for asynchronous
+    #    iteration and consume operations.
+    @active_command = nil
+    @async_callback = nil  # the block passed to an async command
+
+    @read_buffer = ReadBuffer.new
+    @messages = []
+
+    # Dispatch table for handling async responses.
+    @dispatch = []
+    @dispatch[READ_CMD] = method :async_receive_tuple
+    @dispatch[READP_CMD] = method :async_receive_tuple
+    @dispatch[TAKE_CMD] = method :async_receive_tuple
+    @dispatch[TAKEP_CMD] = method :async_receive_tuple
+    @dispatch[TAKE_PRIV_CMD] = method :async_receive_tuple
+    @dispatch[TAKEP_PRIV_CMD] = method :async_receive_tuple
+    @dispatch[READ_ALL_CMD] = method :async_iteration_command
+    @dispatch[TAKE_ALL_CMD] = method :async_iteration_command
+    @dispatch[MONITOR_CMD] = method :async_iteration_command
+    @dispatch[CONSUME_CMD] = method :async_iteration_command
+    @dispatch[MONITOR_STREAM_CMD] = method :async_stream_command
+    @dispatch[CONSUME_STREAM_CMD] = method :async_stream_command
+
     @global_commons_channel = nil  # cached
-    @debug = false
   end
 
   private #===============================================================
@@ -149,7 +198,7 @@ class Client
 	    retval
 	  end
 	end
-	$stderr.printf "sock_recv_io: io=%p\n", io if @debug
+	$stdout.printf "sock_recv_io: io=%p\n", io if @@debug
 	return io
       else
 	raise OperationError, "failed to receive file descriptor"
@@ -162,13 +211,13 @@ class Client
 
   def send(format, *values)
     @reqnum = (@reqnum == REQNUM_MAX ? 1 : @reqnum + 1)
-    $stderr.puts "send: reqnum=" + @reqnum.to_s if @debug
+    $stdout.puts "send: reqnum=" + @reqnum.to_s if @@debug
     send_with_reqnum @reqnum, format, values
   end
 
 
   def send_cont(format, *values)
-    $stderr.puts "send_cont: reqnum=" + @reqnum.to_s if @debug
+    $stdout.puts "send_cont: reqnum=" + @reqnum.to_s if @@debug
     send_with_reqnum @reqnum, format, values
   end
 
@@ -182,19 +231,13 @@ class Client
 
 
   def receive(*expected)
-    payload, file = receive_response
-    decode_response(payload, file, expected)
-  end
-
-
-  def receive_response
     loop do
       length_n = sock_recv 2
       length = length_n.unpack("n").first
       payload = sock_recv length
 
       recv_reqnum, code = payload.unpack("CC")
-      $stderr.puts "received reqnum=" + recv_reqnum.to_s if @debug
+      $stdout.puts "received reqnum=" + recv_reqnum.to_s if @@debug
 
       if code == TUPLE_WITH_RIGHTS_RESP || code == ACCESS_RIGHT_RESP
 	file = sock_recv_io
@@ -203,7 +246,7 @@ class Client
       end
 
       if recv_reqnum == @reqnum
-	return payload[1..-1], file
+	return decode_response payload[1..-1], file, expected
       else
 	file.close rescue nil if file
       end
@@ -212,7 +255,7 @@ class Client
 
 
   def decode_response(payload, file, expected)
-    $stderr.printf "decode_response(payload=%p, file=%p, expected=%p)\n",
+    $stdout.printf "decode_response(payload=%p, file=%p, expected=%p)\n",
       payload, file, expected if @debug
     code = payload.unpack("C").first
     unless code == ERROR_RESP || expected.include?(code)
@@ -295,7 +338,30 @@ class Client
       end
     ensure
       send_cont "C", CANCEL_CMD if needs_cancel
+      @active_command = nil
     end
+  end
+
+
+  def async_receive_tuple(payload, file)
+    code, tuple = decode_response payload, file,
+      [TUPLE_RESP, TUPLE_NIL_RESP, TUPLE_WITH_RIGHTS_RESP]
+    begin
+      @async_callback.call tuple
+    ensure
+      @active_command = nil
+      @async_callback = nil
+    end
+  end
+
+
+  def async_iteration_command
+
+  end
+
+
+  def async_stream_command
+
   end
 
 
@@ -312,6 +378,7 @@ class Client
 
 
   def write(tuple)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_write?
     send "Ca*", WRITE_CMD, YAML.dump(tuple)
     receive_ack
@@ -320,6 +387,7 @@ class Client
 
 
   def reply(tuple)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_write?
     send "Ca*", REPLY_CMD, YAML.dump(tuple)
     receive_ack
@@ -328,12 +396,14 @@ class Client
 
 
   def remember_peer
+    raise ClientError if @active_command
     send "C", REMEMBER_CMD
     receive_handle
   end
 
 
   def forget_peer(peer)
+    raise ClientError if @active_command
     send "CN", FORGET_CMD, peer
     receive_ack
     true
@@ -341,6 +411,7 @@ class Client
 
 
   def write_to(peer, tuple)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_write?
     send "CNa*", WRITE_TO_CMD, peer, YAML.dump(tuple)
     receive_ack
@@ -349,6 +420,7 @@ class Client
 
 
   def forward_to(peer, tuple)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_write? and can_forward?
     send "CNa*", FORWARD_TO_CMD, peer, YAML.dump(tuple)
     receive_ack
@@ -357,6 +429,7 @@ class Client
 
 
   def pass_access_to(peer, file, tuple)
+    raise ClientError if @active_command
     fail "attempt to pass access rights in global tuple space" if is_global?
     raise PrivilegeError unless can_write? and can_pass_access?
     send "CNa*", PASS_ACCESS_TO_CMD, peer, YAML.dump(tuple)
@@ -368,6 +441,7 @@ class Client
 
 
   def read(template)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_read?
     send "Ca*", READ_CMD, YAML.dump(template)
     receive_tuple
@@ -375,6 +449,7 @@ class Client
 
 
   def readp(template)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_read?
     send "Ca*", READP_CMD, YAML.dump(template)
     receive_tuple
@@ -382,6 +457,7 @@ class Client
 
 
   def take(template)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_take?
     send "Ca*", TAKE_CMD, YAML.dump(template)
     receive_tuple
@@ -389,6 +465,7 @@ class Client
 
 
   def takep(template)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_take?
     send "Ca*", TAKEP_CMD, YAML.dump(template)
     receive_tuple
@@ -396,60 +473,75 @@ class Client
 
 
   def take_priv(template)
+    raise ClientError if @active_command
     send "Ca*", TAKE_PRIV_CMD, YAML.dump(template)
     receive_tuple
   end
 
 
   def takep_priv(template)
+    raise ClientError if @active_command
     send "Ca*", TAKEP_PRIV_CMD, YAML.dump(template)
     receive_tuple
   end
 
 
   def read_all(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_read?
+    @active_command = READ_ALL_CMD
     send "Ca*", READ_ALL_CMD, YAML.dump(template)
     execute_iteration true, block
   end
 
 
   def take_all(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_take?
+    @active_command = TAKE_ALL_CMD
     send "Ca*", TAKE_ALL_CMD, YAML.dump(template)
     execute_iteration true, block
   end
 
 
   def monitor(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_read?
+    @active_command = MONITOR_CMD
     send "Ca*", MONITOR_CMD, YAML.dump(template)
     execute_iteration true, block
   end
 
 
   def consume(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_take?
+    @active_command = CONSUME_CMD
     send "Ca*", CONSUME_CMD, YAML.dump(template)
     execute_iteration true, block
   end
 
 
   def monitor_stream(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_read?
+    @active_command = MONITOR_STREAM_CMD
     send "Ca*", MONITOR_STREAM_CMD, YAML.dump(template)
     execute_iteration false, block
   end
 
 
   def consume_stream(template, &block)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_take?
+    @active_command = CONSUME_STREAM_CMD
     send "Ca*", CONSUME_STREAM_CMD, YAML.dump(template)
     execute_iteration false, block
   end
 
 
   def new_binding
+    raise ClientError if @active_command
     send "C", CREATE_NEW_BINDING_CMD
     channel = receive_channel
     channel.hello
@@ -458,6 +550,7 @@ class Client
 
 
   def duplicate
+    raise ClientError if @active_command
     send "C", DUPLICATE_CHANNEL_CMD
     channel = receive_channel
     channel.hello
@@ -466,6 +559,7 @@ class Client
 
 
   def global_commons
+    raise ClientError if @active_command
     unless @global_commons_channel
       send "C", CREATE_GLOBAL_COMMONS_CHANNEL_CMD
       channel = receive_channel
@@ -484,6 +578,7 @@ class Client
   # If {portnum} is nil or 0, then this creates a new region using the next
   # available port number.
   def open_port(portnum=nil, want_global=nil)
+    raise ClientError if @active_command
     raise PrivilegeError unless can_open_any_port?
     portnum ||= 0
     want_global = ((want_global || is_global?) ? 1 : 0)
@@ -493,6 +588,302 @@ class Client
     channel
   end
 
+
+  #=========================================================================
+  # Asynchronous operations
+  #-------------------------------------------------------------------------
+
+  def read_async(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    raise PrivilegeError unless can_read?
+    send "Ca*", READ_CMD, YAML.dump(template)
+    @active_command = @dispatch[READ_CMD]
+    @async_callback = block
+  end
+
+
+  def readp_async(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    raise PrivilegeError unless can_read?
+    send "Ca*", READP_CMD, YAML.dump(template)
+    @active_command = @dispatch[READP_CMD]
+    @async_callback = block
+  end
+
+
+  def take_async(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    raise PrivilegeError unless can_take?
+    send "Ca*", TAKE_CMD, YAML.dump(template)
+    @active_command = @dispatch[TAKE_CMD]
+    @async_callback = block
+  end
+
+
+  def takep_async(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    raise PrivilegeError unless can_take?
+    send "Ca*", TAKEP_CMD, YAML.dump(template)
+    @active_command = @dispatch[TAKEP_CMD]
+    @async_callback = block
+  end
+
+
+  def take_priv(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    send "Ca*", TAKE_PRIV_CMD, YAML.dump(template)
+    @active_command = @dispatch[TAKE_PRIV_CMD]
+    @async_callback = block
+  end
+
+
+  def takep_priv(template, &block)
+    raise ArgumentError, "missing block" unless block
+    raise ClientError if @active_command
+    send "Ca*", TAKEP_PRIV_CMD, YAML.dump(template)
+    @active_command = @dispatch[TAKEP_PRIV_CMD]
+    @async_callback = block
+  end
+
+
+  #-------------------------------------------------------------------------
+  # Management of asynchronous operations by event loop
+  #-------------------------------------------------------------------------
+
+  def fd
+    @sock
+  end
+
+
+  def want_read
+    # XXX may need to be careful; only want read for asynchronous commands
+    @active_command != nil
+  end
+
+
+  def want_write
+    false
+  end
+
+
+  def read_data
+    return unless @sock  # still connected
+
+    @messages.clear
+    @read_buffer.payload ||= ""
+
+    begin
+      data = @sock.read_nonblock READ_SIZE
+      if @@debug
+        $stdout.printf "Client#read_data from %p: %p\n", @sock, data
+      end
+
+      start = 0
+      while start < data.length
+	data_left = data.length - start
+
+	desired_length = @read_buffer.length || MESSAGE_LENGTH_SIZE
+	fill_amount = desired_length - @read_buffer.payload.length
+	usable_amount = [fill_amount, data_left].min
+
+	@read_buffer.payload << data[start, usable_amount]
+	start += usable_amount
+
+	if usable_amount == fill_amount
+	  if @read_buffer.length
+            if @@debug
+              $stdout.printf "Client#read_data: @read_buffer.length = %d\n",
+                @read_buffer.length
+              $stdout.printf "Client#read_data: @read_buffer.payload = %p\n",
+                @read_buffer.payload
+            end
+	    @messages << @read_buffer.payload
+	    @read_buffer.length = nil
+	    @read_buffer.payload = ""
+	  else
+	    @read_buffer.length = @read_buffer.payload.unpack("n").first
+	    @read_buffer.payload = ""
+            if @@debug
+              $stdout.printf "Client#read_data: message length = %d\n",
+                @read_buffer.length
+            end
+	    if @read_buffer.length == 0
+	      raise EOFError, "client protocol error: message length == 0"
+	    end
+	  end
+	end
+      end
+
+    rescue Errno::EINTR  # might be raised by read_nonblock
+      # do nothing, since we'll automatically retry in the next select() round
+
+    # Ruby 1.9.2 preview 2 uses IO::WaitReadable, but earlier versions use
+    # plain Errno::EWOULDBLOCK, so technically we could get rid of
+    # IO::WaitReadable.
+    rescue Errno::EWOULDBLOCK, IO::WaitReadable
+      # IO::WaitReadable shouldn't normally happen since the socket was
+      # ready by the time we performed the read_nonblock; however, a false
+      # readiness notification can lead to this exception.
+      $stdout.printf "Client#read_data from %p: IO::WaitReadable", @sock
+      # do nothing, since we'll automatically retry in the next select() round
+
+    # can get Errno::ECONNRESET
+    rescue SocketError, IOError, EOFError, SystemCallError
+      if @@debug
+        $stdout.printf "Client#read_data from %p: %p", @sock, $!
+      end
+      msg = "lost connection: #{$!.class.name}: #{$!}"
+      shutdown()
+      raise ConnectionBroken, msg
+    end
+
+    @messages.each do |payload|
+      recv_reqnum, code = payload.unpack("CC")
+      $stdout.puts "received reqnum=" + recv_reqnum.to_s if @@debug
+
+      if code == TUPLE_WITH_RIGHTS_RESP || code == ACCESS_RIGHT_RESP
+        fail "UNIMPLEMENTED: receiving fd asynchronously"
+	file = sock_recv_io()
+      else
+	file = nil
+      end
+
+      if recv_reqnum == @reqnum
+        @active_command.call payload[1..-1], file
+      else
+	file.close rescue nil if file
+      end
+    end
+  end
+
+
+  def shutdown
+    return unless @sock
+    @sock.close rescue nil
+    @sock = nil
+  end
+
+
+  def write_data
+    fail "INTERNAL ERROR: Marinda::Client#write_data called"
+  end
+
+end
+
+
+#############################################################################
+
+class ClientEventLoop
+
+  @@debug = false
+
+  def self.debug=(v)
+    @@debug = v
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def initialize
+    @running = false
+    @sources = {}  # source => true
+    @active_sources = {}  # fd => source
+
+    # IO sets to pass to Kernel::select, and IO sets returned by
+    # select.  We use instance variables so that it is easier to inspect
+    # a core dump with gdb.
+    @read_set = []
+    @write_set = []
+    @readable = nil
+    @writable = nil
+  end
+
+
+  # A source should implement the following methods:
+  #
+  #   * fd() => return IO object or nil if source has failed/finished,
+  #   * want_read() => true if the source wants to perform a read,
+  #   * want_write() => true if the source wants to perform a write,
+  #   * read_data() => perform nonblocking read, and
+  #   * write_data() => perform nonblocking write.
+  #
+  def add_source(source)
+    @sources[source] = true
+  end
+
+
+  def remove_source(source)
+    @sources.delete source
+  end
+
+
+  def start
+    @running = true
+    while @running
+      @read_set.clear
+      @write_set.clear
+
+      defunct_sources = []
+      @active_sources.clear
+
+      @sources.each_key do |source|
+        if source.fd
+          if source.want_read || source.want_write
+            @read_set << source.fd if source.want_read
+            @write_set << source.fd if source.want_write
+            @active_sources[source.fd] = source
+          end
+        else
+          defunct_sources << source
+        end
+      end
+
+      defunct_sources.each do |source|
+        @sources.delete source
+      end
+
+      if @@debug
+        $stdout.printf "Client: waiting for I/O ...\n"
+        $stdout.printf "select read_set (%d fds): %p\n",
+          @read_set.length, @read_set
+        $stdout.printf "select write_set (%d fds): %p\n",
+          @write_set.length,@write_set
+      end
+
+      # XXX select can throw "closed stream (IOError)" if a write file
+      #     descriptor has been closed previously; we may want to catch
+      #     this exception and remove the closed descriptor.  Strictly
+      #     speaking, users shouldn't be passing in closed file descriptors.
+      @readable, @writable = select @read_set, @write_set
+      if @@debug
+        $stdout.printf "select returned %d readable, %d writable\n",
+          (@readable ? @readable.length : 0), (@writable ? @writable.length : 0)
+      end
+
+      if @readable
+        @readable.each do |fd|
+          $stdout.printf "readable %p\n", fd if @@debug
+          @active_sources[fd].read_data()
+        end
+      end
+
+      if @writable
+        @writable.each do |fd|
+          $stdout.printf "writable %p\n", fd if @@debug
+          @active_sources[fd].write_data()
+        end
+      end
+    end
+  end
+
+
+  def suspend
+    @running = false
+  end
 
 end
 
